cb569522471dac5ca904d2ea6c485b8eda5247f0
Ansgar Wehrhahn <31626864+AWehrhahn@users.noreply.github.com>
2021-10-22 14:36:52 +0200

    setup pre commit and setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="flex-format",
+    url="https://github.com/AWehrhahn/exoplanet_transit_snr",
+    author="Ansgar Wehrhahn",
+    author_email="ansgar.wehrhahn@physics.uu.se",
+    description="Determine the optimal SNR/ExpTime for exoplanet transit observations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
