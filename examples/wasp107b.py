7337cf19431a70caf1d473fbb19839bfad0db0dd
Ansgar Wehrhahn <31626864+AWehrhahn@users.noreply.github.com>
2022-01-20 13:06:33 +0100

    add some comments

--- examples/wasp107b.py
+++ examples/wasp107b.py
@@ -11,22 +11,31 @@ from exoplanet_transit_snr.snr_estimate import (
 )
 from exoplanet_transit_snr.stellardb import StellarDb
 
+# define the names of the star and planet
+# as well as the datasets within the datasets folder
 star, planet = "WASP-107", "b"
 datasets = {50: "WASP-107b_SNR50", 100: "WASP-107b_SNR100", 200: "WASP-107b_SNR200"}
 
+# Load the nominal data for this star and planet from simbad/nasa exoplanet archive
 sdb = StellarDb()
 star = sdb.get(star)
 planet = star.planets[planet]
 
+# Define the +- range of the radial velocity points,
+# and the density of the sampling
 rv_range = 200
 rv_step = 0.25
 
 for snr in [200]:
+    # Where to find the data, might need to be adjusted
     data_dir = join(dirname(__file__), "../datasets", datasets[snr], "Spectrum_00")
+    # load the data from the fits files, returns several objects
     data = load_data(data_dir, load=True)
+    # Run the cross correlation to the next neighbour
     cc_data = run_cross_correlation(
         data, rv_range=rv_range, rv_step=rv_step, load=True, data_dir=data_dir
     )
+    # Calculate the cohen d value for this cross correlation
     d = calculate_cohen_d_for_dataset(
         data,
         cc_data,
